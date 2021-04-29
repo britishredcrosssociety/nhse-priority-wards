feddd5264d27868686196a0d1140352706b585ce
Matt Thomas <matthewgthomas@gmail.com>
2021-04-29 14:22:07 +0100

    Analyse IMD and rural-urban classifications

--- analyse ward capacity.R
+++ analyse ward capacity.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(geographr)
 library(readxl)
 
 wards <- read_excel("data/Priority and exemplar wards - Healthier Lancashire and South Cumbria.xlsx")
@@ -13,9 +14,36 @@ ward_all_codes <- read_csv("https://github.com/drkane/geo-lookups/raw/master/war
 ward_names <- read_csv("https://opendata.arcgis.com/datasets/63773bdd52e34745be3db659663d5662_0.csv") %>% 
   select(WD17CD, WD17NM)
 
+# Rural-urban classifications in wards
+ruc_wards <- geographr::ruc_england_wales_ward %>% 
+  filter(str_detect(ward_code, "^E"))
+
+# IMD in wards
+imd_wards <- read_csv("https://github.com/matthewgthomas/IMD/raw/master/data/English%20IMD%20-%20Ward%202020.csv")
+
 cni <- 
   cni %>% 
   left_join(ward_names, by = c("Code" = "WD17CD"))
 
 wards %>% 
   left_join(cni, by = c("2019 ward code" = "Code"))
+
+wards_imd_ruc <- wards %>% 
+  left_join(imd_wards, by = c("2019 ward code" = "WD20CD")) %>% 
+  left_join(ruc_wards, by = c("2019 ward code" = "ward_code"))
+
+wards_imd_ruc %>% 
+  select(`2019 CCG code`:Extent, starts_with("Health"), `Proportion of urban LSOAs`:Classification) %>% 
+  write_csv("output/Priority and exemplar wards - Healthier Lancashire and South Cumbria.csv")
+
+# Compare deprivation extent/proportion in priority vs exemplar wards
+wardsimd_ruc %>% 
+  ggplot(aes(x = Status, y = Extent)) +
+  geom_boxplot()
+
+wardsimd_ruc %>% 
+  ggplot(aes(x = Status, y = Health_Extent)) +
+  geom_boxplot()
+
+
+
